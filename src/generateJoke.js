26b4f299dc9fc7df1bece6645261faf1604cb557
Jarda Korinek <korinek14.j@gmail.com>
2022-09-28 17:02:06 +0200

    using axios to access dadjokes api -> jednoducha funkcionalita kompletni -> po nacteni se vygeneruje joke -> pri klikani na button se vygeneruje jiny..

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -1,5 +1,14 @@
-function generateJoke() {
-  return "jokerino";
-}
+const axios = require("axios").default;
 
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  axios.get("https://icanhazdadjoke.com/", config).then((response) => {
+    document.getElementById("joke").innerHTML = response.data.joke;
+  });
+}
 export default generateJoke;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import "./styles/main.scss";
 import laughing from "./assets/Trollface.svg";
 
 const laughImg = document.getElementById("laughImg");
-
 laughImg.src = laughing;
+
+const jokeBtn = document.getElementById("jokeBtn");
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
